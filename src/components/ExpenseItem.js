4406f274d4fd82785f218453bfb553969164aa42
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-11-18 21:10:01 -0300

    #2 CSS Style and Props Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
+import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+
   return (
-    <h2>ExpenseItem</h2>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
   )
 }
 
