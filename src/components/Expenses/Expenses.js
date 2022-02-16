ee6913abd94fb76dd8209a714eef56b3e6004242
Helder Barbosa <helderbarbosa.dev@gmail.com>
2022-02-15 21:45:09 -0300

    #17 Add Expenses Chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,28 +2,31 @@ import React, { useState } from 'react';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
-import ExpensesList from './ExpensesList'
+import ExpensesList from './ExpensesList';
+import ExpertChart from './ExpenseChart';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2021')
+  const [filteredYear, setFilteredYear] = useState('2021');
 
-  const filterChangeHandler = selectedYear => {
-    setFilteredYear(selectedYear)
-  }
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-  const filteredExpenses = props.items.filter(expense => {
-    return expense.date.getFullYear().toString() === filteredYear
-  })
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   return (
     <div>
       <Card className="expenses">
-
-        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpertChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Expenses
+export default Expenses;
